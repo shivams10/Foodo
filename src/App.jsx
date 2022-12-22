66eb9bd59bfdab18097eea8b1c92c98dab5f1b23
Shivam Shukla <shivam@gkmit.co>
2022-12-22 17:26:21 +0530

    add food store

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/navbar";
+import "./App.css";
+
+function App() {
+  const [navVisible, showNavbar] = useState(false);
+  const [cart, setCart] = useState([]);
+  const [warning, setWarning] = useState(false);
+
+  const handleClick = (item)=>{
+	var isPresent = false;
+	cart.forEach((product)=>{
+		if (item.id === product.id){
+			isPresent = true;
+			setWarning(true);
+		}
+	})
+	if (isPresent){
+		alert("item already present")
+		return ;
+	}
+	setCart([...cart, item]);
+}
+
+  return (
+    <>
+      <Navbar visible={navVisible} show={showNavbar} size={cart.length} />
+      <Routes>
+      <Route
+          path="/store"
+          element={
+            <div className={!navVisible ? "page" : "page page-with-navbar"}>
+              <Store handleClick={handleClick} warning={warning}/>
+            </div>
+          }
+        />
+        <Route
+          path="/recipe"
+          element={
+            <div className={!navVisible ? "page" : "page page-with-navbar"}>
+              <Recipe />
+            </div>
+          }
+        />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
--- src/components/productCard/index.jsx
+++ src/components/productCard/index.jsx
@@ -1,9 +1,40 @@
-import"./index.css"
+import { Link } from "react-router-dom";
+import {BiCommentDots} from "react-icons/bi"
+import { BsStarFill } from "react-icons/bs";
 
-const ProductCard = () => {
+import "./index.css";
+
+const ProductCard = (props) => {
+  const { name, price, rating, image, amount, handleClick, id } =
+    props;
+
   return (
-    <div>index</div>
-  )
-}
+    <div className="product-card">
+      <div className="product-image">
+        <img src={image} />
+      </div>
+      <div className="product-info">
+        <h4 className="product-name">{name}</h4>
+        <div className="extra-info">
+          <p className="product-price">₹ {price}</p>
+          <span>
+          <BsStarFill />{rating} 
+            <Link to={`/product/${id}`} className="review-button" props={props}>
+               <BiCommentDots size="16px" />
+            </Link>
+          </span>
+        </div>
+        <button
+          className="button"
+          onClick={() =>
+            handleClick({ name, price, rating, image, amount, id })
+          }
+        >
+          Add to Cart
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default ProductCard
+export default ProductCard;
